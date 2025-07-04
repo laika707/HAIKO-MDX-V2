7a3be4f39f3dd7c87c7b9e4d4f52964c105c729c
laika707 <divincelaika51@gmail.com>
2025-07-04 14:59:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "529633982655",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "243904581829",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "`𝐗𝐓𝐑𝐄𝐌𝐄`",
 // add bot owner name
